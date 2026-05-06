10158664a1724b85e9a9ad785c37810fef7f9919
Mike <69970540+mikerb95@users.noreply.github.com>
2026-05-06 15:11:15 -0500

    Remove unused useLocation import from CrmDashboard.jsx

--- src/pages/CrmDashboard.jsx
+++ src/pages/CrmDashboard.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Ic } from '../components/Icons';
 
 function CrmShell({ active = 'Inicio', children, title, subtitle, actions }) {
